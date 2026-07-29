5ef458c2178b4463ad573cea080366a8cd2e8e63
snigdha-1102 <snigdhasakkuri1102@gmail.com>
2026-07-29 21:44:33 +0530

    Configure Firebase Google Authentication

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBJSU5b8EGUd25I4g53TbXswCgP7uwB9AE",
-  authDomain: "goclean-f62e2.firebaseapp.com",
-  projectId: "goclean-f62e2",
-  storageBucket: "goclean-f62e2.firebasestorage.app",
-  messagingSenderId: "47145277594",
-  appId: "1:47145277594:web:5acbdd2294e0579b5fa723",
-  measurementId: "G-CHWSKSMFB6"
+  apiKey: "AIzaSyCwVC-wF8nRFAobqtnaswfrEfbhu-sadJg",
+  authDomain: "go-clean-26fa7.firebaseapp.com",
+  projectId: "go-clean-26fa7",
+  storageBucket: "go-clean-26fa7.firebasestorage.app",
+  messagingSenderId: "213128728552",
+  appId: "1:213128728552:web:1b308f857d364fab9e1e5d",
+  measurementId: "G-4R3D08C533"
 };
 
 
